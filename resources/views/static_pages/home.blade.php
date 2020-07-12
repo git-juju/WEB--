e923d808fa52b8ba2d0f045726a7859ef0c9aa9c
Libern <libernlin@gmail.com>
2020-07-12 10:46:38 +0000

    wbe

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="jumbotron">
+        <p class="row-cols-md-6">
+            <a class="btn btn-lg
+            btn-success cols-md-6"
+               href="#"
+               role="button">登入</a>
+            <a class="btn btn-lg
+            btn-success cols-md-4"
+               href="#"
+               role="button">登入</a>
+        </p>
+        <p class="row-cols-md-4">
+            <a class="btn btn-lg
+            btn-success"
+               href="#"
+               role="button">登入</a>
+        </p>
+    </div>
+@stop
