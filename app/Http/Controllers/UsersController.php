56bfc5716220845906d90028cfb318a7ac68019b
Libern <libernlin@gmail.com>
2020-07-13 21:11:23 +0000

    用户注册页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Dotenv\Regex\Success;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Mockery\Matcher\Subset;
+use MongoDB\Driver\Session;
 
 class UsersController extends Controller
 {
@@ -16,4 +19,20 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|min:3|max:50',
+            'email' => 'required|email|unique:users|min:11|max:255',
+            'password' => 'required|confirmed|min:6|max:18'
+        ]);
+        $User = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        Session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show',[$User]);
+    }
+
 }
