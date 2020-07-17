d6ad5e0d73004c1f9972319ea94dd92ca9d44035
Libern <libernlin@gmail.com>
2020-07-17 19:40:58 +0000

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,30 +7,15 @@
                 <section class="status_form">
                     @include('shared._status_form')
                 </section>
+                <h4>微博列表</h4>
+                <hr>
+                @include('shared._feed')
             </div>
             <aside class="col-md-4">
                 <section class="user_info">
                     @include('shared._user_info', ['user' => Auth::user()])
                 </section>
             </aside>
-        </div>
-        <div class="jumbotron">
-            <p class="row-cols-md-6">
-                <a class="btn btn-lg
-            btn-success cols-md-6"
-                   href="{{ route('signup') }}"
-                   role="button">登入</a>
-                <a class="btn btn-lg
-            btn-success cols-md-4"
-                   href="{{ route('signup') }}"
-                   role="button">登入</a>
-            </p>
-            <p class="row-cols-md-4">
-                <a class="btn btn-lg
-            btn-success"
-                   href="{{ route('signup') }}"
-                   role="button">登入</a>
-            </p>
         </div>
     @else
         <div class="jumbotron">
