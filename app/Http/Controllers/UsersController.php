1abc02552b16e0b65f36d18a56713ebe9f34452e
Libern <libernlin@gmail.com>
2020-07-17 06:07:32 +0000

    线上QQ邮箱验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -86,13 +86,13 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '896412698@qq.com';
-        $name = '小驹驹！';
+        //$from = '896412698@qq.com';
+        //$name = '小驹驹！';
         $to = $user->email;
         $subject = "感谢注册 畅游中国APP 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ( $to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
     public function confirmEmail($token)
