c7fe180c21da13bfcf6569713a25b7e83bd11254
Libern <libernlin@gmail.com>
2020-07-17 19:27:53 +0000

    微博发布

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','Weibo App')</title>
+    <title>@yield('title','畅游中国')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,22 +1,55 @@
 @extends('layouts.default')
 
 @section('content')
-    <div class="jumbotron">
-        <p class="row-cols-md-6">
-            <a class="btn btn-lg
+    @if (Auth::check())
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+            </div>
+            <aside class="col-md-4">
+                <section class="user_info">
+                    @include('shared._user_info', ['user' => Auth::user()])
+                </section>
+            </aside>
+        </div>
+        <div class="jumbotron">
+            <p class="row-cols-md-6">
+                <a class="btn btn-lg
             btn-success cols-md-6"
-               href="{{ route('signup') }}"
-               role="button">登入</a>
-            <a class="btn btn-lg
+                   href="{{ route('signup') }}"
+                   role="button">登入</a>
+                <a class="btn btn-lg
             btn-success cols-md-4"
-               href="{{ route('signup') }}"
-               role="button">登入</a>
-        </p>
-        <p class="row-cols-md-4">
-            <a class="btn btn-lg
+                   href="{{ route('signup') }}"
+                   role="button">登入</a>
+            </p>
+            <p class="row-cols-md-4">
+                <a class="btn btn-lg
             btn-success"
-               href="{{ route('signup') }}"
-               role="button">登入</a>
-        </p>
-    </div>
+                   href="{{ route('signup') }}"
+                   role="button">登入</a>
+            </p>
+        </div>
+    @else
+        <div class="jumbotron">
+            <p class="row-cols-md-6">
+                <a class="btn btn-lg
+            btn-success cols-md-6"
+                   href="{{ route('signup') }}"
+                   role="button">登入</a>
+                <a class="btn btn-lg
+            btn-success cols-md-4"
+                   href="{{ route('signup') }}"
+                   role="button">登入</a>
+            </p>
+            <p class="row-cols-md-4">
+                <a class="btn btn-lg
+            btn-success"
+                   href="{{ route('signup') }}"
+                   role="button">登入</a>
+            </p>
+        </div>
+    @endif
 @stop
